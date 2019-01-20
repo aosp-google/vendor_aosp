41cef1fbf3d7705e88d893f5b516846de15d8efa
sbwml <984419930@qq.com>
2019-01-20 21:12:49 +0800

    Revert "soong_config: Add TARGET_NEEDS_PLATFORM_TEXT_RELOCATIONS"
    This reverts commit cf7f7d48d0297f9a64413076d755b00e10e48c9a.

--- build/soong/soong_config.mk
+++ build/soong/soong_config.mk
@@ -10,7 +10,6 @@ $(call add_json_bool, Device_support_legacy_hwfde, $(filter true,$(TARGET_LEGACY
 $(call add_json_bool, Device_support_wait_for_qsee, $(filter true,$(TARGET_KEYMASTER_WAIT_FOR_QSEE)))
 $(call add_json_bool, Has_legacy_camera_hal1, $(filter true,$(TARGET_HAS_LEGACY_CAMERA_HAL1)))
 $(call add_json_str, Java_Source_Overlays, $(JAVA_SOURCE_OVERLAYS))
-$(call add_json_bool, Needs_text_relocations, $(filter true,$(TARGET_NEEDS_PLATFORM_TEXT_RELOCATIONS)))
 $(call add_json_str, Specific_camera_parameter_library, $(TARGET_SPECIFIC_CAMERA_PARAMETER_LIBRARY))
 $(call add_json_str_omitempty, Target_process_sdk_version_override, $(TARGET_PROCESS_SDK_VERSION_OVERRIDE))
 $(call add_json_str_omitempty, Target_shim_libs, $(TARGET_LD_SHIM_LIBS))
